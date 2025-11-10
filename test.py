17267fea9b4ea37f99541ed5f3730e9d0e5f95fd
shareitalike <shareitalike@gmail.com>
2025-11-10 14:49:46 +0530

    test.py added

---
+++ test.py
@@ -0,0 +1,18 @@
+#print(1)
+import keyword
+keyword.kwlist
+
+age=25
+print(type(age))
+
+val3=None
+print (type(val3))
+
+s3="""Hello Everyone"""
+print (type(s3))
+
+print(len(s3))
+print(s3[2])
+
+print(s3[-2])
+
