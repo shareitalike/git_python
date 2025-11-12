05f6a2391d8084fcf121e9fabcf2c4510ffff852
shareitalike <shareitalike@gmail.com>
2025-11-12 12:31:06 +0530

    slicing strings2

--- slicing strings.py
+++ slicing strings.py
@@ -19,3 +19,28 @@ print ("john \n20")
 print("This is python's class")
 print('This is python\'s class')
 
+sp="Python is fun"
+language = "python"
+version= "3.13.3"
+print (language + version)
+print(sp * 3,sep =' ')
+
+#Membership in python
+print("python" in sp)
+print("Python" in sp)
+
+#Comparison in strings
+print("python " == "python")
+#Removing spaces from string
+s5= "python "
+s6 = s5.strip()
+print(s6)
+print(s5.strip() == "python")
+
+
+#Replace
+s7 = "We are Learning Python"
+print(s7)
+print(s7.replace("Python","java"))
+print(s7.replace("e","E"))
+print(s7.replace("e","E",1))
